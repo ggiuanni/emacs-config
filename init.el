ee77378e8e0c474bf3486953a9dd9ab8cfcfacb5
Giovanni Cassone <giovanni.csn@gmail.com>
2021-04-06 09:54:49 +0200

    Added org mode and evil mode

--- init.el
+++ init.el
@@ -18,7 +18,7 @@
 
 (set-face-attribute 'default nil :font "CMU Typewriter Text" :height 110)
 (set-face-attribute 'fixed-pitch nil :font "CMU Typewriter Text" :height 1.0)
-(set-face-attribute 'variable-pitch nil :font "CMU Serif" :height 1.0)
+(set-face-attribute 'variable-pitch nil :font "CMU Serif" :height 1.1)
 
 (column-number-mode)
 (global-display-line-numbers-mode t)
@@ -67,7 +67,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(treemacs-projectile projectile lsp-ivy lsp-treemacs company-box company company-lsp company-mode magit lsp-mode which-key counsel ivy-rich ivy rainbow-delimiters doom-themes doom-modeline use-package)))
+   '(evil-collection evil org-journal yasnippet-snippets yasnippet visual-fill-column org-bullets treemacs-projectile projectile lsp-ivy lsp-treemacs company-box company company-lsp company-mode magit lsp-mode which-key counsel ivy-rich ivy rainbow-delimiters doom-themes doom-modeline use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -166,6 +166,134 @@
   :config
   (setq projectile-project-search-path '("C:/dev/"))
   :bind (:map projectile-mode-map
-	      ("C-c p" . projectile-command-map)))
+	      ("C-c p" . projectile-command-map))
+  :custom((projectile-completion-system 'ivy)))
 
 (use-package treemacs-projectile)
+
+;; Evil mode
+(defun ggiuanni/evil-hook ()
+  (dolist (mode '(custom-mode
+		  eshell-mode
+		  git-rebase-mode
+		  erc-mode
+		  circe-server-mode
+		  circe-chat-mode
+		  circe-query-mode
+		  sauron-mode
+		  term-mode))
+    (add-to-list 'evil-emacs-state-modes mode)))
+
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-C-i-jump nil)
+  ;; :hook (evil-mode . ggiuanni/evil-hook)
+  :config
+  (evil-mode 1)
+  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+
+  ;; Use visual line motions even outside of visual-line-mode buffers
+  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+
+  (evil-set-initial-state 'messages-buffer-mode 'normal)
+  (evil-set-initial-state 'dashboard-mode 'normal))
+
+(use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init))
+
+;; Org mode
+(defun ggiuanni/org-mode-setup ()
+  (org-indent-mode)
+  (variable-pitch-mode 1)
+  (visual-line-mode 1))
+
+(defun ggiuanni/org-font-setup ()
+  (dolist (face '((org-document-title . 2.0)
+		  (org-level-1 . 1.8)
+		  (org-level-2 . 1.7)
+		  (org-level-3 . 1.6)
+		  (org-level-4 . 1.5)
+		  (org-level-5 . 1.4)
+		  (org-level-6 . 1.3)
+		  (org-level-7 . 1.2)
+		  (org-level-8 . 1.1)))
+  (set-face-attribute (car face) nil :font "CMU Serif" :weight 'regular :height (cdr face)))
+
+;; Ensure that anything that should be fixed-pitch in Org files appears that way
+(set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
+(set-face-attribute 'org-code nil   :inherit '(shadow fixed-pitch))
+(set-face-attribute 'org-table nil   :inherit '(shadow fixed-pitch))
+(set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+(set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+(set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+(set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch))
+
+(use-package org
+  :hook (org-mode . ggiuanni/org-mode-setup)
+  :config
+  (setq org-ellipsis " ▾"
+	org-hide-emphasis-markers t)
+
+  (setq org-agenda-start-with-log-mode t)
+  (setq org-log-done 'time)
+  (setq org-log-into-drawer t)
+
+  (setq org-agenda-files
+	(directory-files-recursively "~/Google Drive/Agenda" "\\.org$"))
+
+  (setq org-refile-targets
+	'((org-agenda-files :maxlevel . 1)))
+
+  (setq org-capture-templates
+    `(("t" "Tasks / Projects")
+      ("tt" "Task" entry (file+olp "~/Google Drive/Agenda/Main.org" "Inbox")
+       "* TODO %?" :empty-lines 1)
+      ("q" "Quick note" entry (file+olp "~/Google Drive/Post-it/QuickNotes.org" "Random")
+           "* %?" :empty-lines 1))
+    )
+
+  (define-key global-map (kbd "C-c t")
+    (lambda () (interactive) (org-capture nil "tt")))
+
+  (define-key global-map (kbd "C-c q")
+    (lambda () (interactive) (org-capture nil "q")))
+
+
+  (which-key-add-key-based-replacements
+    "C-c t" "Capture task")
+
+  (ggiuanni/org-font-setup))
+
+(use-package org-bullets
+  :after org
+  :hook (org-mode . org-bullets-mode))
+
+(defun ggiuanni/org-mode-visual-fill ()
+  (setq visual-fill-column-width 100
+	visual-fill-column-center-text t)
+  (visual-fill-column-mode 1))
+
+(use-package visual-fill-column
+  :hook (org-mode . ggiuanni/org-mode-visual-fill))
+
+(use-package yasnippet
+  :diminish yas-minor-mode
+  :config
+  (use-package yasnippet-snippets)
+  (yas-global-mode 1))
+
+(use-package org-journal
+  :ensure t
+  :defer t
+  :init
+  ;; Change default prefix key; needs to be set before loading org-journal
+  (setq org-journal-prefix-key "C-c j")
+  :config
+  (setq org-journal-dir "~/Google Drive/journal"))
