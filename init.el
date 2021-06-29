3191dcd067cfc3cb1ff2202157690dcf758a4be4
Giovanni <giovanni.csn@gmail.com>
2021-06-29 09:44:03 +0200

    Capture random thoughts

--- init.el
+++ init.el
@@ -219,7 +219,9 @@ If the new path's directories does not exist, create them."
    (directory-files-recursively "~/Polymath/Agenda" "\\.org$"))
   (org-capture-templates
       '(("t" "Todo" entry (file+headline "~/Polymath/Agenda/Inbox.org" "Tasks")
-         "* TODO %?\n  %i"))))
+         "* TODO %?\n  %i")
+	("r" "Random Thought" entry (file+headline "~/Polymath/Agenda/Inbox.org" "Random Thoughts")
+	 "* %?\n %i"))))
 
 (use-package org-bullets
   :after org
